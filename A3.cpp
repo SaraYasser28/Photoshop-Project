246b4c34066d78f0934d8497b17c67dc27879789
SaraYasser28 <161871991+SaraYasser28@users.noreply.github.com>
2024-03-27 12:14:43 +0200

    Update A3.cpp
    added helpful comments and handled the infinite loop when entering alphabet for choices using limits

--- A3.cpp
+++ A3.cpp
@@ -2,6 +2,7 @@
 #include "Image_Class.h"
 #include <fstream>
 #include <string>
+#include <limits>
 using namespace std;
 
 // Function prototypes
@@ -13,9 +14,9 @@ void blackAndWhite(Image& image);
 int main() {
     string filename;
     Image image;
-    bool imageLoaded = false;
+    bool imageLoaded = false; // Flag to track if an image is loaded
 
-    // Menu loop
+    // Menu
     while (true) {
         cout << "\nMenu:\n";
         cout << "\n1) Load Image\n2) Flip Image\n3) Apply Black and White Filter\n4) Save Image\n5) Exit\n";
@@ -23,6 +24,9 @@ int main() {
 
         int choice;
         cin >> choice;
+
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
         switch (choice) {
             case 1: {
@@ -34,14 +38,15 @@ int main() {
                         cout << "\n**File '" << filename << "' does not exist. Please enter a valid filename**\n";
                     } else {
                         image.loadNewImage(filename);
-                        cout << "\nImage loaded successfully.\n";
-                        imageLoaded = true;
+                        cout << "\nImage loaded successfully!!\n";
+                        imageLoaded = true; // Set flag to true indicating image is loaded
                         break;
                     }
                 }
                 break;
             }
             case 2: {
+                // Flip option
                 if (!imageLoaded) {
                     cout << "\n**Please load an image first**\n";
                     break;
@@ -53,6 +58,9 @@ int main() {
                 cout << "\nEnter your choice: ";
                 int flipChoice;
                 cin >> flipChoice;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
                 switch (flipChoice) {
                     case 1:
                         flipHorizontal(image.imageData, image.width, image.height, image.channels);
@@ -68,6 +76,7 @@ int main() {
                 break;
             }
             case 3: {
+                // Black and white filter option
                 if (!imageLoaded) {
                     cout << "\n**Please load an image first**\n";
                     break;
@@ -78,28 +87,33 @@ int main() {
                 break;
             }
             case 4: {
+                // Save image option
                 if (!imageLoaded) {
-                    cout << "\n**Please load an image first.**\n";
+                    cout << "\n**Please load an image first**\n";
                     break;
                 }
 
-                string saveOption;
+                int saveOption;
                 cout << "\n1) Save as " << filename << "\n2) Save as a new file\n";
                 cout << "\nEnter your choice: ";
                 cin >> saveOption;
-
-                if (saveOption == "1") {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                switch (saveOption)
+                {
+                case '1':
                     image.saveImage(filename);
-                    cout << "\nImage saved as " << filename << ".\n";
-                } else if (saveOption == "2") {
+                    cout << "\nImage saved as " << filename << "!!\n";
+                case '2':
                     cout << "\nEnter the new image name: ";
                     cin >> filename;
                     image.saveImage(filename);
-                    cout << "\nImage saved as " << filename << ".\n";
-                } else {
+                    cout << "\nImage saved as " << filename << "!!\n";
+                    break;
+                default:
                     cout << "\n**Invalid choice. Please enter '1' or '2'**\n";
+                    break;
                 }
-                break;
             }
             case 5:
                 cout << "\nExiting program ...\n";
@@ -118,11 +132,14 @@ bool fileExists(const string& filename) {
     return file.good();
 }
 
-// Flip image horizontally
+// Function to flip the image horizontally
 void flipHorizontal(unsigned char* imageData, int width, int height, int channels) {
     unsigned char temp;
+    // Iterate over each row of the image
     for (int row = 0; row < height; ++row) {
+        // Iterate over each column up to the middle of the image width
         for (int column = 0; column < width / 2; ++column) {
+            // Swap corresponding pixels from left and right sides of the image
             for (int c = 0; c < channels; ++c) {
                 temp = imageData[(row * width + column) * channels + c];
                 imageData[(row * width + column) * channels + c] = imageData[(row * width + (width - 1 - column)) * channels + c];
@@ -132,11 +149,14 @@ void flipHorizontal(unsigned char* imageData, int width, int height, int channel
     }
 }
 
-// Flip image vertically
+// Function to flip the image vertically
 void flipVertical(unsigned char* imageData, int width, int height, int channels) {
     unsigned char temp;
+    // Iterate over each row up to the middle of the image height
     for (int row = 0; row < height / 2; ++row) {
+        // Iterate over each column of the image
         for (int column = 0; column < width; ++column) {
+            // Swap corresponding pixels from top and bottom sides of the image
             for (int c = 0; c < channels; ++c) {
                 temp = imageData[(row * width + column) * channels + c];
                 imageData[(row * width + column) * channels + c] = imageData[((height - 1 - row) * width + column) * channels + c];
@@ -146,16 +166,20 @@ void flipVertical(unsigned char* imageData, int width, int height, int channels)
     }
 }
 
-// Black and white filter
+// Function to apply the black and white filter to the image
 void blackAndWhite(Image& image) {
+    // Iterate over each pixel in the image
     for (int i = 0; i < image.width; ++i) {
         for (int j = 0; j < image.height; ++j) {
+            // Iterate over each color channel (RGB channels)
             for(int k = 0; k < 3; ++k){
+                // If the pixel value is greater than 127, set it to white (255)
                 if(image(i, j, k) > 127){
                     image(i, j, 0) = 255;
                     image(i, j, 1) = 255;
                     image(i, j, 2) = 255;
                 }
+                // Otherwise, set it to black (0)
                 else{
                     image(i, j, 0) = 0;
                     image(i, j, 1) = 0;
